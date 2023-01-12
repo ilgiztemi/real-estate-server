081777125afa49925c86f45dab5dd8e3f467e51a
Kanykey Albanova <temirbekov91@gmail.com>
2023-01-12 14:54:42 -0500

    endpoints are changed

--- endpoints/addsEndpoints.js
+++ endpoints/addsEndpoints.js
@@ -8,14 +8,14 @@ const {
   deleteAdd,
 } = require("../handlers/addsHandlers");
 
-router.get("/api/adds", (req, res) => getAdds(req, res));
+router.get("/adds", (req, res) => getAdds(req, res));
 
-router.get("/api/add/:add", (req, res) => getAdd(req, res));
+router.get("/add/:add", (req, res) => getAdd(req, res));
 
-router.post("/api/add/add", (req, res) => addNewAdd(req, res));
+router.post("/add/add", (req, res) => addNewAdd(req, res));
 
-router.put("/api/update", (req, res) => updateAdd(req, res));
+router.put("/update", (req, res) => updateAdd(req, res));
 
-router.delete("/api/delete", (req, res) => deleteAdd(req, res));
+router.delete("/delete", (req, res) => deleteAdd(req, res));
 
 module.exports = router;
